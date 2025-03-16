a6b713e125b121c3dbc2989175f45835d57b42ec
AlexKhabibov <alex.lv.u@gmail.com>
2025-03-16 15:57:20 +0300

    study_TS

---
+++ studyTS.js
@@ -0,0 +1,47 @@
+// function sum(a: number, b: number): number {
+//     return a + b;
+//   }
+//   console.log(sum(10, 5));
+// type Person = {
+//     name: string;
+// }
+// type Employee = {
+//     id: number;
+// }
+// type Manager = Person & Employee;
+// const manager: Manager = {
+//     name: 'Alex',
+//     id: 1
+// }
+// console.log(manager);
+// function identity<T>(value: T): T {
+//     return value;
+// }
+// console.log(identity(34));
+// class Box<T> {
+//     private content: T;
+//     constructor(content: T) {
+//         this.content = content;
+//     }
+//     getContent(): T {
+//         return this.content;
+//     }
+// };
+// const box1 = new Box<string>('box number one');
+// const box2 = new Box<number>(2);
+// console.log(box1);
+// console.log(box2);
+// console.log(box1.getContent());
+// console.log(box2.getContent());
+
+
+
+// function greet(name) {
+//     return "\u041F\u0440\u0438\u0432\u0435\u0442, ".concat(name, "!");
+// }
+// console.log(greet('2'));
+// // console.log(greet(123)); // Ошибка: TS скажет, что нужен string
+
+
+
+
